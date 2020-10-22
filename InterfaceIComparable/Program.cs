9cecb34bcb85b17e64c08a160cc5473c4beabf7d
PEMMC <pauloeduardomagaldi@hotmail.com>
2020-10-21 23:49:00 -0300

    Adicionar arquivos de projeto.

---
+++ InterfaceIComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.IO;
+
+namespace InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\apagar\Projects\InterfaceIComparable\employees.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee (sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
